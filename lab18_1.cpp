1dcc2fac19bb3dbc69a405d06a97811e8c70d565
PEEMTM <76035600+PEEMTM@users.noreply.github.com>
2021-02-23 16:51:07 +0700

    finish

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,23 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double max(double x,double y){
+	if(x>y){
+		return x;
+	}else return y;
+}
 
+double min(double x,double y){
+	if(x<y){
+		return x;
+	}else return y;
+}
 
-
+double overlap(Rect R1,Rect R2){
+	double area,L1,L2;
+	L1=min(R1.x+R1.w,R2.x+R2.w)-max(R1.x,R2.x);
+	L2=min(R1.y,R2.y)-max(R1.y-R1.h,R2.y-R2.h);
+	area=L1*L2;
+	if(area<0) area=0;
+	return area;
 }
